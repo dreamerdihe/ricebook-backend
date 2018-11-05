0a9a6336bf34bdb7bff18f846a1f47229bd531b4
Dreamer_he <sysuheyuan@gmail.com>
2018-11-05 10:58:35 -0600

    add the function of post articles

--- src/articles.js
+++ src/articles.js
@@ -2,8 +2,8 @@ const Posts = require('../model/post')
 const Profiles = require('../model/profile')
 const mongoose = require('mongoose')
 
-function getArticle(req, res) {
-    // Implement the function of getting articles
+// Implement the function of getting articles
+function getArticle(req, res) {    
     const id = req.params.id
     if (!id) {
         console.log("one request for getting feed")
@@ -32,31 +32,35 @@ function getArticle(req, res) {
             })
         })
     } else {
+        // check whether the id is valid
         if(mongoose.Types.ObjectId.isValid(id)) {
             Posts.findById(id, (err, post) => {
                 if (err) {
                     console.log(err)
                     return res.sendStatus(404)
                 }
+                // first assume the id is a post id
                 if (post) {
                     console.log("one request for getting post by post id")
                     return res.send({articles: post})
                 } else {
+                    // if it is not a post id, we assume it is a user id
                     console.log("one request for getting post by user id")
                     Profiles.findById(id, (err, user) => {
                         if (err) {
                             console.log(err)
                             return res.sendStatus(404)
                         }
+                        // if we can't find the user, we response an empty array
                         if (!user) {
-                            return res.sendStatus(404)
+                            return res.send({"articles": []})
                         } else {
                             Posts.find().where('author.username').equals(user.username).exec((err, posts) => {
                                 if (err) {
                                     console.log(err)
                                 return res.sendStatus(404)
                                 }
-                                return res.send({articles: posts})
+                                return res.send({"articles": posts})
                             })
                         }
                     })
@@ -68,12 +72,29 @@ function getArticle(req, res) {
     }
 }
 
+// Implement the function of editting a specific articles
 function editArticle(req, res) {
-    // Implement the function of editting a specific articles
+    
 }
 
+// Implement the function of posting an article
 function postArticle(req, res) {
-    // Implement the function of posting an article
+    console.log(req.username + " requst for post article")
+    const text = req.body.text
+    Profiles.findOne({username: req.username}, (err, user) => {
+        if (err) {
+            console.log(err)
+            return res.status(404).send({'result': 'err'})
+        }
+        const author = {id: user.id, username: req.username}
+        Posts.create({author: author, body: text, comments: []}, (err, post) => {
+            if (err) {
+                console.log(err)
+                return res.status(404).send({'result': 'err'})
+            } 
+            return res.status(200).send({"articles": post})
+        })
+    })
 }
 
 module.exports = (app, isloggedin) => {
